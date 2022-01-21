84aeb9a5c3a0053f00ee404310c5d4ee766bed47
benjo457 <29567784+benjo457@users.noreply.github.com>
2022-01-21 18:30:57 +0100

    Create App.java

---
+++ hlc-parent/hlc-util/src/main/java/hlc/util/App.java
@@ -0,0 +1,18 @@
+package hlc.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+    public static void main( String[] args ) {
+        LOG.info("HLC");
+        System.out.println( "Hello World!" );
+    }
+}
